3a700687089b86d8802dec8922e792fae9584d66
Diva Angelika Mulia <diva.angelika.mulia@gmail.com>
2025-08-22 23:53:01 +0700

    done

---
+++ Plus Minus - Easy.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    arr_len = len(arr)
+    pos = 0
+    neg = 0
+    null = 0
+
+    for i in range(arr_len):
+        if arr[i] > 0: # pos
+            pos += 1
+        if arr[i] < 0: # min
+            neg += 1
+        if arr[i] == 0:
+            null += 1
+    
+    # return pos/arr_len, neg/arr_len, min/arr_len
+    # return f"{pos/arr_len:.6f}\n{neg/arr_len:.6f}\n{null/arr_len:.6f}"
+    print(f"{pos/arr_len:.6f}")
+    print(f"{neg/arr_len:.6f}")
+    print(f"{null/arr_len:.6f}")
+
+# print(plusMinus([-4, 3, -9, 0, 4, 1]))
+plusMinus([-4, 3, -9, 0, 4, 1])
+
+
+# if __name__ == '__main__':
+#     n = int(input().strip())
+
+#     arr = list(map(int, input().rstrip().split()))
+
+#     plusMinus(arr)
