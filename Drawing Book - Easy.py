e6f73ecc146c6924cb336376e06bf176c480b74f
Diva Angelika Mulia <diva.angelika.mulia@gmail.com>
2025-09-29 00:45:38 +0700

    done

--- Drawing Book - Easy.py
+++ Drawing Book - Easy.py
@@ -20,31 +20,7 @@ def pageCount(n, p):
     ctr_end = 0
     ctr_p = 0
     ctr_n = 0
-    # res_start = 0
-    # res_end = 0
 
-    # # if p == 1:
-    # #     ctr_start = 0
-    # #     ctr_end = 0
-    
-    # for i in range(0,p):
-    #     ctr_start += 1
-    #     # if p == i:
-    #     #     ctr += 1
-
-    # for k in range(n,p,-1):
-    #     ctr_end += 1
-    
-    # res_start = abs(int((n/p)-ctr_start))
-
-    # if ctr_end%2 == 0 or ctr_end == 1:
-    #     # ctr_end = abs(int((n/p)-ctr_end-1))
-    #     res_end = abs(int((n/p)-ctr_end-1))
-    # # else:
-    # #     res_end = abs(int((n/p)-ctr_end))
-    #     # ctr_end = abs(int((n/p)-ctr_end))
-    
-    # return res_start, res_end
     if p == 1:
         return 0
     else:
@@ -57,23 +33,6 @@ def pageCount(n, p):
             return ctr_start
         else:
             return ctr_end
-
-
-
-
-        
-
-    
-    # elif res_start < res_end:
-    #     return res_start
-    # else:
-    #     return res_end
-
-
-    
-    # return abs(ctr_start-p), abs(ctr_end-p)
-    # return ctr_start, ctr_end
-    # return abs(int((n/p)-ctr_start)), abs(int((n/p)-ctr_end))
 
 # print(pageCount(6,2)) #1 #2
 # print(pageCount(5,4)) #2 #0
