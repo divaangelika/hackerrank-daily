fdaa7e416cc1981f328fc63be2961712c7ec899b
Diva Angelika Mulia <diva.angelika.mulia@gmail.com>
2025-10-05 00:50:38 +0700

    done

--- Electronics Shop - Easy.py
+++ Electronics Shop - Easy.py
@@ -19,7 +19,7 @@ def getMoneySpent(keyboards, drives, b):
             if keyboards[i] < b and drives[j] < b:
                 is_on_budget = keyboards[i] + drives[j]
 
-                if is_on_budget <= b:
+                if is_on_budget <= b and is_on_budget > max:
                     max = is_on_budget
                 else:
                     continue
@@ -29,10 +29,47 @@ def getMoneySpent(keyboards, drives, b):
     
     return max
 
-print(getMoneySpent([3,1],[5,2,8],10))
-print(getMoneySpent([4],[5],5))
-print(getMoneySpent([40,50,60],[5,8,12],60))
-print(getMoneySpent([183477,732159,779867],[598794,596985,156054],374625))
+# Test cases
+keyboards = [
+    12, 25, 37, 41, 19, 22, 35, 50, 60, 75,
+    10, 15, 20, 30, 40, 45, 55, 65, 70, 80,
+    5, 7, 9, 11, 13, 16, 18, 21, 23, 26,
+    28, 29, 31, 32, 33, 34, 36, 38, 39, 42,
+    43, 44, 46, 47, 48, 49, 51, 52, 53, 54,
+    56, 57, 58, 59, 61, 62, 63, 64, 66, 67,
+    68, 69, 71, 72, 73, 74, 76, 77, 78, 79,
+    81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+    91, 92, 93, 94, 95, 96, 97, 98, 99, 100
+]
+
+drives = [
+    3, 5, 7, 9, 11, 13, 15, 17, 19, 21,
+    2, 4, 6, 8, 10, 12, 14, 16, 18, 20,
+    22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+    32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
+    42, 43, 44, 45, 46, 47, 48, 49, 50, 51,
+    52, 53, 54, 55, 56, 57, 58, 59, 60, 61,
+    62, 63, 64, 65, 66, 67, 68, 69, 70, 71,
+    72, 73, 74, 75, 76, 77, 78, 79, 80, 81,
+    82, 83, 84, 85, 86, 87, 88, 89, 90, 91,
+    92, 93, 94, 95, 96, 97, 98, 99, 100
+]
+
+budget = 150
+
+print(getMoneySpent(keyboards, drives, budget))
+
+# print(getMoneySpent([3, 1], [5, 2, 8], 10))   # Expected: 9
+# print(getMoneySpent([4], [6], 5))             # Expected: -1
+# print(getMoneySpent([40, 50, 60], [5, 8, 12], 60))  # Expected: 58
+# print(getMoneySpent([10, 12], [5, 3], 15))    # Expected: 15
+# print(getMoneySpent([1, 2, 3], [2, 3, 4], 9)) # Expected: 7
+# print(getMoneySpent([21, 22, 23], [30, 40], 20)) # Expected: -1
+
+# print(getMoneySpent([3,1],[5,2,8],10))
+# print(getMoneySpent([4],[5],5))
+# print(getMoneySpent([40,50,60],[5,8,12],60))
+# print(getMoneySpent([183477,732159,779867],[598794,596985,156054],374625))
 
 # if __name__ == '__main__':
 #     fptr = open(os.environ['OUTPUT_PATH'], 'w')
